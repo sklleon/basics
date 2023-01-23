4f353691826eeab2ac4cb86deec1ab3c2d61a2b8
sklleon <100928959+sklleon@users.noreply.github.com>
2023-01-23 17:19:02 +0300

    lesson-4

--- lesson-4/task_4_6.py
+++ lesson-4/task_4_6.py
@@ -4,3 +4,25 @@
 # Обратите внимание, что создаваемый цикл не должен быть бесконечным. Предусмотрите условие его завершения. ####
 # Например, в первом задании выводим целые числа, начиная с 3.
 # При достижении числа 10 — завершаем цикл. Вторым пунктом необходимо предусмотреть условие, при котором повторение элементов списка прекратится.
+
+from itertools import count, cycle
+
+
+def func_number(start_number, stop_number):
+    for el in count(start_number):
+        if el > stop_number:
+            break
+        else:
+            print(el)
+
+
+def func_cycle(my_list, iteration):
+    i = 0
+    iter = cycle(my_list)
+    while i < iteration:
+        print(next(iter))
+        i += 1
+
+
+func_number(start_number=int(input('Введите число старта: ')), stop_number=int(input('Введите число остановки: ')))
+func_cycle(my_list=[1, 2], iteration=int(input('Введите итерацию: ')))
