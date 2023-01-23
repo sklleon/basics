280e2643e7fe8b90be5712a7758cb308e464923d
sklleon <100928959+sklleon@users.noreply.github.com>
2023-01-23 14:07:52 +0300

    lesson-4 at 23-01-2023

--- lesson-4/task_4_5.py
+++ lesson-4/task_4_5.py
@@ -1,3 +1,40 @@
 # 5. Реализовать формирование списка, используя функцию range() и возможности генератора. В список должны войти чётные числа от 100 до 1000 (включая границы).
 # Нужно получить результат вычисления произведения всех элементов списка.
 # Подсказка: использовать функцию reduce().
+
+from functools import reduce
+from itertools import count
+
+
+# version 1
+def generator():
+    for el in range(100, 1001, 2):
+        yield el
+
+
+g = generator()
+
+
+def my_func(prev_el, g):
+    return prev_el * g
+
+
+print(reduce(my_func, g))
+
+# version 2
+my_list = [el for el in range(100, 1001) if el % 2 == 0]
+
+
+def my_func(prev_el, el):
+    return prev_el * el
+
+
+print(reduce(my_func, my_list))
+
+# version 3
+# my_list2 = [el for el in count(100) if el % 2 == 0 and el < 1001]
+#
+#
+# def my_func2(prev_el, el):
+#     return prev_el * el
+# print(reduce(my_func2, my_list2))
