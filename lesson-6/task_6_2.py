e601664ec7494366cf710c0fd984f6a57fd64889
sklleon <100928959+sklleon@users.noreply.github.com>
2023-02-14 12:17:28 +0300

    lesson-6 changes up at 12-02-2023

--- lesson-6/task_6_2.py
+++ lesson-6/task_6_2.py
@@ -8,16 +8,20 @@
 # Например: 20 м*5000 м*25 кг*5 см = 12500 т.
 
 class Road:
+    self.weight = 25
 
-    def __init__(self, length, width):
-        self._length = length
-        self._width = width
-        self.weight = 25
-        self.height = 5
 
-    def asphalt_mass(self):
-        asphalt_mass = self._length * self._width * self.weight * self.height / 1000
-        print(f'Для покрытия всего дорожного полотна неободимо {round(asphalt_mass)} массы асфальта')
+self.height = 5
+
+
+def __init__(self, length, width):
+    self._length = length
+    self._width = width
+
+
+def asphalt_mass(self):
+    asphalt_mass = self._length * self._width * self.weight * self.height / 1000
+    print(f'Для покрытия всего дорожного полотна потребуются {round(asphalt_mass)} тонн асфальта')
 
 
 obj = Road(5000, 20)
